3e7e13bf36843860e472e3096a18ca589fa991cd
Evgheni-Cernev <los11g12@gmail.com>
2019-08-11 15:51:49 +0300

    git users card

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import  "./App.css"
+import UserInformation from './UserInformation'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      users:[],
+      oldUsers:[],
+      isLoaded: false,
+      value: "",
+    }
+  }
+
+  componentDidMount(){
+    let url = "https://api.github.com/users"
+  
+    fetch(url)
+      .then(res => res.json())
+      .then(json => {
+         this.setState({
+           isLoaded: true,
+           users: json,
+           oldUsers: json
+         }) 
+      });  
+  }
+ 
+  render() {
+      let { oldUsers, isLoaded } = this.state
+
+      if(!isLoaded){
+        return <div>Loading...</div>;
+      } else {
+
+      const handlerInputValue = (event) =>{
+        let search = this.state.users.filter( element => {
+         return element.login.indexOf(event.target.value) !== -1
+        })
+        this.setState({ 
+          oldUsers: search,
+          value:event.target.value
+        })
+      }
+
+   
+
+      const cards =  oldUsers.map(element => {
+        return ( 
+        <div className="cards">
+          <div className="names">
+            {element.login}
+          </div>
+          <img className="ava" src={element.avatar_url}/>
+          <button>more information</button>
+        </div>
+        )
+      })
+
+      return (
+        <div className="wrapper">
+          <div className = "blockCard">
+              <label>
+                  <input 
+                    className="search"
+                    type="text" 
+                    value={this.state.value} 
+                    onChange={handlerInputValue}
+                  />
+                  Search
+              </label>
+            {cards}
+          </div>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
---
+++ src/UserInformation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function UserInformation() {
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default UserInformation;
